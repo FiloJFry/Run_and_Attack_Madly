025808e50861a601ec7067810443100bedcd8c00
FiloJFry <filippomandolini14@gmail.com>
2026-05-15 17:46:02 +0200

    Refactor Nemico and Arma classes with new methods

--- DefinizioneClassi.js
+++ DefinizioneClassi.js
@@ -260,21 +260,152 @@ class Arma
     {
         return this._Rumori;
     }
-    Fuoco()
+ constructor(nome,danni,portata,munizioni,rateo,velocità,altorateo,mirino,Rumori)
     {
-        this.Spara();
+        this._nome = nome;
+        this._danni = danni;
+        this._portata = portata;
+        this._munizioni = munizioni;
+        this._maxmunizioni = munizioni;
+        this._inventario = 4*munizioni;
+        this._rateo = rateo;
+        this._altorateo = altorateo;
+        this._velocità = velocità;
+        this._mirino = mirino;
+        this._Rumori = Rumori;
+    }
+    set nome(name)
+    {   
+        this._nome = name;
+    }
+    get nome()
+    {
+        return this._nome;
+    }
+    set danni(danni)
+    {
+        this._danni = danni;
+    }
+    get danni()
+    {
+        return this._danni;
+    }
+    set portata(portata)
+    {
+        this._portata = portata;
+    }
+    get portata()
+    {
+        return this._portata;
+    }
+    set munizioni(ammo)
+    {
+        this._munizioni = ammo;
+    }
+    get munizioni()
+    {
+        return this._munizioni;
+    }
+    set inventario(ammo)
+    {
+        this._inventario = ammo;
+    }
+    get inventario()
+    {
+        return this._inventario;
+    }
+    set rateo(rateo)
+    {
+        this._rateo = rateo;
+    }
+    get rateo()
+    {
+        return this._rateo;
+    }
+    set altorateo(altorateo)
+    {
+        this._altorateo = altorateo;
+    }
+    get altorateo()
+    {
+        return this._altorateo;
+    }
+    set velocità(v)
+    {
+        this._velocità = v;
+    }
+    get velocità()
+    {
+        return this._velocità;
+    }
+    set maxmunizioni(riferimento)
+    {
+        this._maxmunizioni = riferimento;
+    }
+    get maxmunizioni()
+    {
+        return this._maxmunizioni;
+    }
+    set mirino(testo)
+    {
+        this._mirino = testo;
+    }
+    get mirino()
+    {
+        return this._mirino;
+    }
+    set Rumori(suoni)
+    {
+        this._Rumori = suoni; 
+    }
+    get Rumori()
+    {
+        return this._Rumori;
+    }
+    Fuoco(NemicoScelto)
+    {
+        this.Spara(NemicoScelto);
         Colpo = true;
         if(this.altorateo)
         {   
-            Spara = setInterval(() => {this.Spara();},this.rateo + 20);
+            Spara = setInterval(() => {this.Spara(NemicoScelto);},ArmaPresa.rateo + 20);
         }
-        else
+        else if(risparo)
         {   
             risparo = false;
             let cont = 0;
             gap = setInterval(() => {if(!InPausa){cont += 100; if(cont >= this.rateo + 100){clearInterval(gap); risparo = true;}}},100);
         }
     }
+    Arresta()
+    {
+        Colpo = false;
+        if(this.altorateo)
+        {
+            if(Spara != undefined)
+            {
+                clearInterval(Spara);
+                Spara = undefined;
+            }
+        }
+    }
+    Spara(NemicoScelto)
+    {    
+        if(this.munizioni > 0)
+        { 
+            ArmaInCanna.setAttribute('src',`./Immagini/Armi/${this.nome}_attaccando.jpg`);
+            this.munizioni -= 1;
+            PiuInfo.textContent = `${this.munizioni}|${this.inventario}`;
+            RumoriArma.textContent = `${this.Rumori[0]}`; 
+            setTimeout(() => {Preso(this,distanza,NemicoScelto);},1000*distanza/this.velocità);
+            setTimeout(() => {ArmaInCanna.setAttribute('src',`./Immagini/Armi/${this.nome}.jpg`); RumoriArma.textContent = "";},20);
+        }
+        else
+        {   
+            PiuInfo.style.color = 'red';
+            setTimeout(() => {if(Giocando){PiuInfo.style.color = 'white';}},1000);
+        }
+}
     Arresta()
     {
         Colpo = false;
@@ -364,10 +495,16 @@ class Arma
 }
 class Mischia extends Arma
 {   
-    constructor(nome,danni,portata,munizioni,rateo,altorateo,mirino,Rumori)
+    constructor(nome,danni,portata,munizioni,rateo,velocità,altorateo,mirino,Rumori)
     {
-        super(nome,danni,portata,munizioni,rateo,altorateo,mirino,Rumori);
+        super(nome,danni,portata,munizioni,rateo,velocità,altorateo,mirino,Rumori);
+        this._munizioni = 1;
         this._inventario = 0;
+        this._rateo = 45000;
+        this._velocità = Infinity;
+        this._altorateo = false;
+        this._mirino = munizioni;
+        this._Rumori = rateo;
     }
     Spara()
     {
